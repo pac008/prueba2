ea2ddf712f73b753d189ddc79838023c7bbbf133
Miguel <miguelhalc@gmail.com>
2020-07-02 23:02:21 +0200

    server actualizado

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use( require('./servidor/routes/usuario'));
 
 
-mongoose.connect('mongodb://localhost:27017/cafe', {
+mongoose.connect(process.env.URLDB, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true
